49db8d8f67e4942e75e90107025b070b7ab77651
rami <ramisaabmail@gmail.com>
2017-11-08 16:08:55 -0500

    Delete restLength.m

--- restLength.m
+++
@@ -1,9 +0,0 @@
-function [rest_length] = restLength (index_value,fs)
-%UNTITLED3 Summary of this function goes here
-%   Detailed explanation goes here
-
-rest_in_seconds = 0.2;
-rest_length = 0.2*fs;
-
-end
-
